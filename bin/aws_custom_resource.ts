c2323c4bab7a82a25467d81a0bae1afbaa913f4a
Stuart Forrest <stuart.forrest@infinityworks.com>
2023-01-04 14:17:19 +0000

    Implement CustomResource to deploy/manage an EventBridge rule

--- bin/aws_custom_resource.ts
+++ bin/aws_custom_resource.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AwsCustomResourceStack } from '../lib/aws_custom_resource-stack';
+import * as cdk from "aws-cdk-lib";
+import "source-map-support/register";
+import { AwsCustomResourceStack } from "../lib/aws_custom_resource-stack";
 
 const app = new cdk.App();
-new AwsCustomResourceStack(app, 'AwsCustomResourceStack', {
+new AwsCustomResourceStack(app, "AwsCustomResourceStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new AwsCustomResourceStack(app, 'AwsCustomResourceStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { region: "eu-west-2" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/aws_custom_resource-stack.ts
+++ lib/aws_custom_resource-stack.ts
@@ -1,16 +1,67 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { EventBus } from "aws-cdk-lib/aws-events";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import {
+  AwsCustomResource,
+  AwsCustomResourcePolicy,
+  PhysicalResourceId,
+} from "aws-cdk-lib/custom-resources";
+import { Construct } from "constructs";
 
 export class AwsCustomResourceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bus = new EventBus(this, "EventBus", {
+      eventBusName: "CustomResourceTestEventBus",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCustomResourceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const eventBridgeService = "EventBridge";
+    const ruleName = "CustomResourceRule";
+    const resourceId = "RuleFromCustomResource";
+    const rule = {
+      Name: ruleName,
+      Description: ruleName,
+      EventBusName: bus.eventBusName,
+      EventPattern: JSON.stringify({
+        Source: ["AnySource"],
+        Detail: {
+          hello: [null],
+        },
+      }),
+    };
+    new AwsCustomResource(this, "RuleCustomResource", {
+      onCreate: {
+        service: eventBridgeService,
+        action: "putRule",
+        parameters: rule,
+        physicalResourceId: PhysicalResourceId.of(resourceId),
+      },
+      onUpdate: {
+        service: eventBridgeService,
+        action: "putRule",
+        parameters: rule,
+        physicalResourceId: PhysicalResourceId.of(resourceId),
+      },
+      onDelete: {
+        service: eventBridgeService,
+        action: "deleteRule",
+        parameters: {
+          Name: ruleName,
+          EventBusName: bus.eventBusName,
+        },
+      },
+      policy: AwsCustomResourcePolicy.fromStatements([
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: ["events:PutRule", "events:DeleteRule"],
+          resources: [
+            `arn:aws:events:${Stack.of(this).region}:${
+              Stack.of(this).account
+            }:rule/${bus.eventBusName}/${ruleName}`,
+          ],
+        }),
+      ]),
+    });
   }
 }
