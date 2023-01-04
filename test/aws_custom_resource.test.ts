70df93b34c35aa5e1b0d0309a1873fc58f36951f
Stuart Forrest <stuart.forrest@infinityworks.com>
2023-01-04 10:47:57 +0000

    Initial commit

---
+++ test/aws_custom_resource.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCustomResource from '../lib/aws_custom_resource-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_custom_resource-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCustomResource.AwsCustomResourceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
